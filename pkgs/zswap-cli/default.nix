e9d061658f82205a94d6e3aee1fc10adfc9e96c7
ElXreno <elxreno@gmail.com>
2020-04-24 10:02:39 +0300

    Updated zswap-cli to 1.0.3

--- pkgs/zswap-cli/default.nix
+++ pkgs/zswap-cli/default.nix
@@ -2,21 +2,21 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "zswap-cli";
-  version = "1.0.2";
+  version = "1.0.3";
 
   src = fetchFromGitHub {
     owner = "ElXreno";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1z8m52hj6n3vp5ficwdwqn493hm4g1bhfwr953nqqqn32fj9hgnw";
+    sha256 = "01kpziwmb22ldsmgia0s7rb38ah6xbz5wivyh6lr4mgb76kx1g09";
   };
 
-  cargoSha256 = "0skkxzvj6rqkik5br0q31lpbddfh1zm35ch1xb61yhllxjqp0b3i";
+  cargoSha256 = "1qrxz9lmrz5f4jbgc1j4amx2j1hl91dv7jarhn38syb9bpkxga38";
 
   meta = with stdenv.lib; {
     description = "Utility for controlling zswap parameters";
     homepage = "https://github.com/ElXreno/zswap-cli";
-    license = licenses.asl20;
+    license = licenses.mpl20;
     maintainers = [ ];
     platforms = platforms.all;
   };
